90d3a51967c41a2d58ea0a476dfd81a64e35e1c8
jlangaa <joshua.a.langfus@gmail.com>
2020-02-18 11:49:14 -0500

    added more documentation to gen.test.data function

--- gen_test_data.R
+++ gen_test_data.R
@@ -2,8 +2,13 @@
 # Josh Langfus
 # Feb 19 2020
 
-## Generate a test data frame with 6 variables
+## Generate a test data frame with 6 nested variables
 ##
+## Usage:
+## gen.test.data(50)
+## outputs a test data frame with 50 observations per condition
+##
+## Output:
 ## ID: participant ID
 ## Diagnosis: the type of diagnosis
 ## Measure: the form used to measure symptoms
