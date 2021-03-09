fdd5518ccca317cd35492093aae19a9eed72c504
bellete <54329480+bellete@users.noreply.github.com>
2021-03-09 17:37:28 -0500

    Idk what I'm doing...It looks too bad but it can print all the three samples now

--- R/print.bouldr.R
+++ R/print.bouldr.R
@@ -20,13 +20,15 @@ print.bouldr <- function(x, tests = FALSE, ...) {
   cat("Predictor var:\t", rhs, "\n")
   }
 
-  if (length(rhs) > 1){
+  if (length(as.character(rhs[[2]])) == 1){
     cat("Predictor var:\t", rhs[[2]], "\n")
     cat("Grouping var:\t", rhs[[3]], "\n")
   }
 
-  if (length(rhs) > 3){
-    cat("Faceting var:\t", as.character(rhs), "\n")
+  if (length(as.character(rhs[[2]])) == 3){
+    cat("Predictor var:\t", as.character(rhs[[2]])[[2]], "\n")
+    cat("Grouping var:\t", as.character(rhs[[2]])[[3]], "\n")
+    cat("Faceting var:\t", rhs[[3]], "\n")
   }
 
   cat("\n\nAUC table\nSee next page\n\n")
