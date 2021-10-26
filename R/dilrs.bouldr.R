68dad04533f4311bcbd9d76612dedb427bac8479
jlangaa <joshua.a.langfus@gmail.com>
2021-10-25 20:49:37 -0400

    fixed export global variables warnings on pkg check

--- R/dilrs.bouldr.R
+++ R/dilrs.bouldr.R
@@ -12,6 +12,7 @@
 #'
 #' @return a numeric vector containing the classification statistics
 #' @export
+#' @importFrom rlang .data
 dlrs <- function(x, scores, outcomes, positive, ...) {
   # Computes Diagnostic Likelihood ratios (and a bunch of other stuff)
   #
@@ -27,7 +28,7 @@ dlrs <- function(x, scores, outcomes, positive, ...) {
 
   dots <- list(...)
   ## make sure x is data.table object
-  x <- as_tibble(x)
+  x <- tidyr::as_tibble(x)
   if (!is.null(dots$cut)) {
     ## DiLR formula:
     ## LR+ = P(Score > Cut | Yes Dx) / P(Score > Cut | No Dx)
@@ -37,22 +38,21 @@ dlrs <- function(x, scores, outcomes, positive, ...) {
     cut <- dots$cut
 
     ret <- x %>%
-      summarise(
+      dplyr::summarise(
         cut = cut,
-        true_pos = sum(get(scores) >= cut &
-                         get(outcomes) == positive),
-        false_pos = sum(get(scores) >= cut &
-                          get(outcomes) != positive),
-        true_neg = sum(get(scores) < cut &
-                         get(outcomes) != positive),
-        false_neg = sum(get(scores) < cut &
-                          get(outcomes) == positive),
-        sensitivity = true_pos / (true_pos + false_neg),
-        specificity = true_neg / (false_pos + true_neg),
-        LR_pos = (true_pos / (true_pos + false_neg)) / (false_pos / (false_pos + true_neg)),
-        LR_neg = (false_neg / (true_pos + false_neg)) / (true_neg / (false_pos + true_neg))
+        true_pos = sum(.data[[scores]] >= cut &
+                         .data[[outcomes]] == positive),
+        false_pos = sum(.data[[scores]] >= cut &
+                          .data[[outcomes]] != positive),
+        true_neg = sum(.data[[scores]] < cut &
+                         .data[[outcomes]] != positive),
+        false_neg = sum(.data[[scores]] < cut &
+                          .data[[outcomes]] == positive),
+        sensitivity = .data$true_pos / (.data$true_pos + .data$false_neg),
+        specificity = .data$true_neg / (.data$false_pos + .data$true_neg),
+        LR_pos = (.data$true_pos / (.data$true_pos + .data$false_neg)) / (.data$false_pos / (.data$false_pos + .data$true_neg)),
+        LR_neg = (.data$false_neg / (.data$true_pos + .data$false_neg)) / (.data$true_neg / (.data$false_pos + .data$true_neg))
       )
-
 
     ## return as a tibble
     return(ret)
@@ -63,17 +63,17 @@ dlrs <- function(x, scores, outcomes, positive, ...) {
     tot.pos <- sum(x[[outcomes]] == positive)
     tot.neg <- sum(x[[outcomes]] != positive)
 
-    quantiles <- quantile(x[[scores]], seq(0, 1, 1 / n))
+    quantiles <- stats::quantile(x[[scores]], seq(0, 1, 1 / n))
 
     ret <- x %>%
-      mutate(score.band = cut(get(scores), breaks = quantiles, ordered_result = TRUE)) %>%
-      group_by(score.band) %>%
-      summarize(
-        positive.cases = sum(get(outcomes) == positive),
-        negative.cases = n() - positive.cases,
-        percent.positive = positive.cases / tot.pos,
-        percent.negative = negative.cases / tot.neg,
-        LR = percent.positive / percent.negative,
+      dplyr::mutate(score.band = cut(get(scores), breaks = quantiles, ordered_result = TRUE)) %>%
+      dplyr::group_by(.data$score.band) %>%
+      dplyr::summarize(
+        positive.cases = sum(.data[[outcomes]] == positive),
+        negative.cases = dplyr::n() - .data$positive.cases,
+        percent.positive = .data$positive.cases / tot.pos,
+        percent.negative = .data$negative.cases / tot.neg,
+        LR = .data$percent.positive / .data$percent.negative,
         total.positive = tot.pos,
         total.negative = tot.neg
       )
